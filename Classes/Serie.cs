1ca4479e39160d297926eeb4325160585bfa2677
Felipe <contaanonimaevil@gmail.com>
2021-02-21 15:54:35 -0300

    2/5 OK

---
+++ Classes/Serie.cs
@@ -0,0 +1,37 @@
+namespace DIO.Series
+{
+    public class Serie : EntidadeBase
+    {
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            Id = id;
+            Genero = genero;
+            Titulo = titulo;
+            Descricao = descricao;
+            Ano = ano;
+        }
+
+        private Genero Genero {get; set;}
+        private string Titulo {get; set;}
+        private string Descricao {get; set;}
+        private int Ano {get; set;}
+
+        public override string ToString() {
+            string retorno = "";
+            retorno += "Id: " + Id.ToString();
+            retorno += " | Genero: " + Genero;
+            retorno += " | Titulo: " + Titulo;
+            retorno += " | Descricao: " + Descricao;
+            retorno += " | Ano: " + Ano.ToString();
+            return retorno;
+        }
+
+        public string GetTitulo() {
+            return Titulo;
+        }
+
+        public int GetId() {
+            return Id;
+        }
+    }
+}
